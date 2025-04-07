2ac4502fbf9c8a6252d298908a1be482ab81cafb
gnlwi <1sxy36744@fzsmall.io>
2025-04-07 09:28:22 +0800

    Create stat.js

---
+++ images/stat.js
@@ -0,0 +1,26 @@
+(function() {
+    var scripts = document.getElementsByTagName('script');
+    var currentScript = scripts[scripts.length - 1];
+    var token = currentScript.getAttribute('data-token');
+    
+    if (!token) {
+        console.error('统计代码缺少token参数');
+        return;
+    }
+    
+    function sendStat() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/stat.php?token=' + encodeURIComponent(token) + 
+                 '&page=' + encodeURIComponent(window.location.href) + 
+                 '&title=' + encodeURIComponent(document.title) + 
+                 '&sw=' + screen.width + 
+                 '&sh=' + screen.height, true);
+        xhr.send();
+    }
+    
+    if (window.addEventListener) {
+        window.addEventListener('load', sendStat, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', sendStat);
+    }
+})(); 
